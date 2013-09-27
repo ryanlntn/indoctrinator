51b69078326e28d790e571a59c3e8705091e1b39
Ryan Linton <ryanlntn@gmail.com>
2013-09-27 11:36:16 -0700

    Make title default to nil.

--- lib/project/tutorial.rb
+++ lib/project/tutorial.rb
@@ -1,11 +1,12 @@
 module Indoctrinator
   class Tutorial < ICETutorialController
-    def self.new(pages)
+
+    def self.new(pages, args={})
       tutorial = self.alloc.initWithNibName("ICETutorialController_iPhone", bundle: nil) 
       tutorial.pages = pages
-      # turorial.title = nil
-      # tutorial.setButton1Block() 
-      # tutorial.setButton2Block()
+      tutorial.title = args[:title]
+      # tutorial.setButton1Block(lambda {nil}) 
+      # tutorial.setButton2Block(lambda {nil})
       $t = tutorial
       tutorial
     end
@@ -14,20 +15,24 @@ module Indoctrinator
       self.setPages(Array(pages))
     end
 
-    # def title=(title)
-    #   self.setOverlayTitle {
-    #     _overlayTitle.setText(title)
-    #   }
-    # end
-
-    def setOverlayTitle
-      @_overlayTitle.setText(nil)
+    def title=(label)
+      return self.remove_title unless label
+      if label.is_a?(String)
+        self.title.text = label if self.title
+      else
+        self.remove_title
+        self.view.addSubview label
+      end
     end
+
+    def title
+      @title ||= self.view.subviews.find{ |v| v.is_a?(UILabel) }
+    end
+
+    def remove_title
+      self.title.removeFromSuperview if self.title && self.title.respond_to?(:removeFromSuperview)
+      @title = nil
+    end
+
   end
 end
-#       self.setOverlayTitle(_overlayTitle.setText(title))
-
-# - (void)setOverlayTitle{
-#     // ...or change by an UIImageView if you need it.
-#     [_overlayTitle setText:@"Welcome"];
-# }
