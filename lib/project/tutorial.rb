09d959c0d996be333414292a4c953dd9b8db0ab9
Ryan Linton <ryanlntn@gmail.com>
2013-09-27 16:54:52 -0700

    Abstract remove methods into one remove method that removes arbitrary subviews.

--- lib/project/tutorial.rb
+++ lib/project/tutorial.rb
@@ -22,22 +22,17 @@ module Indoctrinator
     end
 
     def title=(label)
-      return self.remove_title unless label
+      return self.remove(:title) unless label
       if label.is_a?(String)
         self.title.text = label if self.title
       else
-        self.remove_title
+        self.remove(:title)
         self.view.addSubview label
       end
     end
 
     def title
       @title ||= self.view.subviews.find{ |v| v.is_a?(UILabel) }
-    end
-
-    def remove_title
-      self.title.removeFromSuperview if self.title && self.title.respond_to?(:removeFromSuperview)
-      @title = nil
     end
 
     def remove_buttons
@@ -53,11 +48,6 @@ module Indoctrinator
       @button1
     end
 
-    def remove_button1
-      self.button1.removeFromSuperview if self.button1 && self.button1.respond_to?(:removeFromSuperview)
-      @button1 = nil
-    end
-
     def button2=(button)
       @button2 = button
       self.view.addSubview @button2
@@ -67,10 +57,9 @@ module Indoctrinator
       @button2
     end
 
-    def remove_button2
-      self.button2.removeFromSuperview if self.button2 && self.button2.respond_to?(:removeFromSuperview)
-      @button2 = nil
+    def remove(v)
+      self.send(v).removeFromSuperview if self.send(v) && self.send(v).respond_to?(:removeFromSuperview)
+      self.instance_variable_set "@#{v}", nil
     end
-
   end
 end
